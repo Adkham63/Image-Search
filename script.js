a20e978c69f9df0514d6d2f6810541d48029db45
Karimov Adkham <130540336+Adkham63@users.noreply.github.com>
2024-01-24 23:51:51 +0500

    script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let page = 1;
 
 async function searchImage(){
     inputData = inputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const url = `Access Key from Unsplash`;
 
     const response = await fetch(url);
     const data = await response.json();
